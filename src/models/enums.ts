0ff371b51c906feadd77a70ffcf642bc58c5d341
oalexandru <oalexandru@deloittece.com>
2019-06-11 12:50:18 +0300

    Added method descriptions and extracted some constants into variables

--- src/models/enums.ts
+++ src/models/enums.ts
@@ -6,3 +6,11 @@ export enum ActivityType {
 export enum TimeZone {
     LONDON = "Europe/London"
 }
+
+export enum RetroConstants {
+    INITIAL_ACTIVITY_DETAILS_CAPACITY = 11,
+    TEMPLATE_LAST_ROW = 39,
+    TEMPLATE_FIRST_ROW_AFTER_ACTIVITY_DETAILS = 29,
+    TEMPLATE_LAST_COLUMN = 17,
+    TEMPLATE_FIRST_COLUMN = 1
+}
