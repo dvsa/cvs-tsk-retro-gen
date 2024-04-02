52b4551c0f0a22966a6349b7906d82b3616da5c8
naathanbrown <nathancjb@googlemail.com>
2024-04-02 11:02:07 +0100

    feat(cb2-10603): add new process for SQS records

--- src/utils/sqsProcess.ts
+++ src/utils/sqsProcess.ts
@@ -1,7 +1,7 @@
 import DynamoDB from "aws-sdk/clients/dynamodb";
 
 export const processRecord = (record: any) => {
-  const recordBody = JSON.parse(record.body).Message;
+  const recordBody = JSON.parse(JSON.parse(record.body).Message);
   if (recordBody.eventName === "MODIFY" && recordBody.dynamodb && recordBody.dynamodb.NewImage) {
     return DynamoDB.Converter.unmarshall(recordBody.dynamodb.NewImage);
   }
