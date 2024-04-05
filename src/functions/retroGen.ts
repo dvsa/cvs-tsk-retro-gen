c496adb34555502dba1d3547c117c54c1204d0a2
Nathan Brown <nathancjb@googlemail.com>
2024-04-05 15:11:01 +0100

    feat(cb2-10603): update activities to use a pipe (#109)
    * feat(cb2-10603): add new process for SQS records
    
    * feat(cb2-10603): add new process for SQS records
    
    * feat(cb2-10603): add new process for SQS records
    
    * feat(cb2-10603): add new process for SQS records
    
    * feat(cb2-10603): add new process for SQS records
    
    * feat(cb2-10603): update to use common microservice function

--- src/functions/retroGen.ts
+++ src/functions/retroGen.ts
@@ -1,13 +1,14 @@
+import { processRecord } from "@dvsa/cvs-microservice-common/functions/sqsFilter";
+import { Lambda } from "aws-sdk";
 import { ManagedUpload } from "aws-sdk/clients/s3";
-import { RetroGenerationService } from "../services/RetroGenerationService";
-import { ERRORS } from "../assets/Enum";
-import { SharePointAuthenticationService } from "../services/SharePointAuthenticationService";
-import { SharePointService } from "../services/SharePointService";
 import * as rp from "request-promise";
-import { TestResultsService } from "../services/TestResultsService";
+import { ERRORS } from "../assets/Enum";
 import { ActivitiesService } from "../services/ActivitiesService";
 import { LambdaService } from "../services/LambdaService";
-import { Lambda } from "aws-sdk";
+import { RetroGenerationService } from "../services/RetroGenerationService";
+import { SharePointAuthenticationService } from "../services/SharePointAuthenticationService";
+import { SharePointService } from "../services/SharePointService";
+import { TestResultsService } from "../services/TestResultsService";
 
 /**
  * λ function to process a DynamoDB stream of test results into a queue for certificate generation.
@@ -26,15 +27,18 @@ const retroGen = async (event: any): Promise<void | ManagedUpload.SendData[]> =>
   const sharePointService = new SharePointService(rp);
 
   event.Records.forEach((record: any) => {
-    const visit: any = JSON.parse(record.body);
-    const retroUploadPromise = retroService.generateRetroReport(visit).then(async (generationServiceResponse: { fileName: string; fileBuffer: Buffer }) => {
-      const tokenResponse = await sharepointAuthenticationService.getToken();
-      const accessToken = JSON.parse(tokenResponse).access_token;
-      const sharePointResponse = await sharePointService.upload(generationServiceResponse.fileName, generationServiceResponse.fileBuffer, accessToken);
-      return sharePointResponse;
-    });
+    const recordBody = JSON.parse(JSON.parse(record.body).Message);
+    const visit: any = processRecord(recordBody);
+    if (visit) {
+      const retroUploadPromise = retroService.generateRetroReport(visit).then(async (generationServiceResponse: { fileName: string; fileBuffer: Buffer }) => {
+        const tokenResponse = await sharepointAuthenticationService.getToken();
+        const accessToken = JSON.parse(tokenResponse).access_token;
+        const sharePointResponse = await sharePointService.upload(generationServiceResponse.fileName, generationServiceResponse.fileBuffer, accessToken);
+        return sharePointResponse;
+      });
 
-    retroUploadPromises.push(retroUploadPromise);
+      retroUploadPromises.push(retroUploadPromise);
+    }
   });
 
   return Promise.all(retroUploadPromises).catch((error: any) => {
